d79069fce7a25eada4520fa216a2fbe334374181
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-07-03 22:11:33 +0530

    Socket.io

--- ProjectV01/src/controllers/AuthController.js
+++ ProjectV01/src/controllers/AuthController.js
@@ -1,5 +1,5 @@
 const authModel = require("../models/AuthModel");
-
+const jwt = require("jsonwebtoken");
 const doLogin = async (req, resp) => {
   try {
     let body = req.body;
@@ -10,9 +10,24 @@ const doLogin = async (req, resp) => {
         message: "User doesn't exist in database",
       });
     } else {
+      // JWT Implementation
+      const config = {
+        jwt: {
+          secret: "LPU@summer-training-2025", //Secret Key
+          expiresIn: "24h",
+        },
+      };
+      const token = jwt.sign(
+        { userId: userData.id, email: userData.email },
+        config.jwt.secret,
+        { expiresIn: config.jwt.expiresIn }
+      );
+
       return resp.status(200).json({
         success: true,
-        message: userData,
+        message: "Login Successful", 
+        userData: userData,
+        token
       });
     }
   } catch (error) {
